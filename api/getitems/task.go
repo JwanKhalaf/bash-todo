e05882fa63d6fe02609e2887b4739d864cf57343
Jwan Khalaf <jwan.khalaf@outlook.com>
2022-09-12 14:19:36 +0100

    add json struct tags

--- api/getitems/task.go
+++ api/getitems/task.go
@@ -1,8 +1,9 @@
 package getitems
 
 type Task struct {
-	TaskId    string `dynamodbav:"task_id"`
-	Content   string `dynamodbav:"content"`
-	CreatedAt string `dynamodbav:"created_at"`
-	IsDone    bool   `dynamodbav:"is_done"`
+	TaskId    string `dynamodbav:"task_id" json:"task_id"`
+	UserId    string `dynamodbav:"user_id" json:"user_id"`
+	Content   string `dynamodbav:"content" json:"content"`
+	CreatedAt string `dynamodbav:"created_at" json:"created_at"`
+	IsDone    bool   `dynamodbav:"is_done" json:"is_done"`
 }
