cd8bf72a956422db36df3ebf033ed6a1ed3519ba
Jwan Khalaf <jwan.khalaf@outlook.com>
2022-10-15 12:08:03 +0100

    improve handler test for listing tasks

--- api/tasks/list/handler_test.go
+++ api/tasks/list/handler_test.go
@@ -1,12 +1,41 @@
 package list
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-func TestHandler(t *testing.T) {
-	got := "foo"
-	want := "bar"
+    "github.com/jwankhalaf/bash-todo/api/tasks"
+)
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+func TestListTasksHandler(t *testing.T) {
+  // create the task store
+  taskStore := tasks.NewTaskStore()
+
+  // create a request to pass to our handler
+  request, err := http.NewRequest("POST", "/", nil)
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  // we create a response recorder which satisfies http.responsewriter to
+  // to record the response
+  response := httptest.NewRecorder()
+
+  handler := GetListTasksHandler(taskStore)
+
+  // our handler satisfies http.handler, so we can call its serve http method
+  // directly and pass in our request and response recorder
+  handler.ServeHTTP(response, request)
+
+  // check the status code is what we expect
+  if status := response.Code; status != http.StatusOK {
+    t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+  }
+
+  // check the response body is what we expect
+  expected := `[]`
+  if response.Body.String() != expected {
+    t.Errorf("handler returned unexpected body: got %v want %v", response.Body.String(), expected)
+  }
 }
